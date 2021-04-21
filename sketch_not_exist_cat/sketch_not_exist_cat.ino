e93107548a301371314adfe10d42bc66639e65ce
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-04-21 10:40:21 +0900

    add: Flashing LED when updating

--- sketch_not_exist_cat/sketch_not_exist_cat.ino
+++ sketch_not_exist_cat/sketch_not_exist_cat.ino
@@ -1,6 +1,8 @@
 #include "DigiKeyboard.h"
 
 void setup() {
+  pinMode(1, OUTPUT);
+  
   DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
   delay(1000);
@@ -13,15 +15,21 @@ void setup() {
   DigiKeyboard.print("https\'//thiscatdoesnotexist.com/");
   delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  delay(1500);
+  delay(2000);
 
   // 画面を最大化する
   DigiKeyboard.sendKeyStroke(KEY_F11);
-  delay(500);
 }
 
 void loop() {
-  // 10秒おきに更新をかける
+  // 10秒待機
   delay(10000);
+  digitalWrite(1, HIGH);
+
+  // 更新をかける
   DigiKeyboard.sendKeyStroke(KEY_F5);
+  delay(100);
+
+  // LED消灯
+  digitalWrite(1, LOW);  
 }
