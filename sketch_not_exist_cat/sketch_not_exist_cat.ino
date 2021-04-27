f1a1b9d497755e4de5b2edcbb2448b38b9a89faf
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2021-04-27 20:14:55 +0900

    fix: Sometimes it's not recognized

--- sketch_not_exist_cat/sketch_not_exist_cat.ino
+++ sketch_not_exist_cat/sketch_not_exist_cat.ino
@@ -5,17 +5,17 @@ void setup() {
   
   DigiKeyboard.update();
   DigiKeyboard.sendKeyStroke(0);
-  delay(1000);
+  DigiKeyboard.delay(1000);
   
   // 「ファイル名を指定して実行」を起動
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
-  delay(1000);
+  DigiKeyboard.delay(1000);
 
   // 存在しない猫を見せる
   DigiKeyboard.print("https\'//thiscatdoesnotexist.com/");
-  delay(500);
+  DigiKeyboard.delay(500);
   DigiKeyboard.sendKeyStroke(KEY_ENTER);
-  delay(2000);
+  DigiKeyboard.delay(2000);
 
   // 画面を最大化する
   DigiKeyboard.sendKeyStroke(KEY_F11);
@@ -23,12 +23,12 @@ void setup() {
 
 void loop() {
   // 10秒待機
-  delay(10000);
+  DigiKeyboard.delay(10000);
   digitalWrite(1, HIGH);
 
   // 更新をかける
   DigiKeyboard.sendKeyStroke(KEY_F5);
-  delay(100);
+  DigiKeyboard.delay(100);
 
   // LED消灯
   digitalWrite(1, LOW);  
